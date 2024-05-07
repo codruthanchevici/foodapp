05e2ab7439ef2ffe959b78efaf2f771210f318e2
codruthanchevici <hanchevicicodrut@gmail.com>
2024-05-07 20:47:17 +0300

    stilizare detalii

--- src/components/FoodDetail.jsx
+++ src/components/FoodDetail.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+import styles from "./fooddetail.module.css";
 export default function FoodDetail({ foodId }) {
   const [food, setFood] = useState({});
   const [isLoading, setIsLoading] = useState(true);
@@ -17,27 +17,31 @@ export default function FoodDetail({ foodId }) {
   }, [foodId]);
   return (
     <div>
-      Food Details: {foodId}
-      <div>
-        <h1>{food.title}</h1>
-        <img src={food.image} />
-      </div>
-      <div>
-        <span>
-          <strong>{food.readyInMinutes} Minutes</strong>
-        </span>
-        <span>{food.vegetarian ? "Vegatarian" : "Non-vegetarian"}</span>
-      </div>
-      <div>
-        <span>{food.pricePerServing / 100} Per Serving</span>
-      </div>
-      <div>
+      <div className={styles.recipeCard}>
+        <h1 className={styles.recipeName}>{food.title}</h1>
+        <img className={styles.recipeImage} src={food.image} />
+
+        <div className={styles.recipeDetails}>
+          <span>
+            <strong>{food.readyInMinutes} Minutes</strong>
+          </span>
+          <span>{food.vegetarian ? "Vegatarian" : "Non-vegetarian"}</span>
+        </div>
+        <div>
+          <span>{food.pricePerServing / 100} Per Serving</span>
+        </div>
         <h2>Instructions</h2>
-        {isLoading ? (
-          <p>Loading...</p>
-        ) : (
-          food.analyzedIntructions[0].steps.map((step) => <li>{step.step}</li>)
-        )}
+        <div className={styles.recipeInstructions}>
+          <ol>
+            {isLoading ? (
+              <p>Loading...</p>
+            ) : (
+              food.analyzedIntructions[0].steps.map((step) => (
+                <li>{step.step}</li>
+              ))
+            )}
+          </ol>
+        </div>
       </div>
     </div>
   );
